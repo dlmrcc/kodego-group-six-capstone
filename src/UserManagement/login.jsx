0470d1cf1d9228d0de9b2c65e6289253950d2ff8
Reffin <ryancarbonel1984@gmail.com>
2023-03-09 16:07:25 +0800

    modified login.jsx

--- src/UserManagement/login.jsx
+++ src/UserManagement/login.jsx
@@ -5,6 +5,7 @@ function Login() {
     return (
 
         <>
+
             <div class="h-screen font-sans login bg-cover">
                 <div class="container mx-auto h-full flex flex-1 justify-center items-center">
                     <div class="w-full max-w-lg">
