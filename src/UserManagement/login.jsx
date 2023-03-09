2eb7e3fde3eb78740a5c94cb93de310c319ad60c
Reffin <ryancarbonel1984@gmail.com>
2023-03-09 16:00:36 +0800

    modified login.jsx

--- src/UserManagement/login.jsx
+++ src/UserManagement/login.jsx
@@ -1,49 +1,49 @@
 
 function Login() {
 
-  
+
     return (
-    
-            <>
-<div class="h-screen font-sans login bg-cover">
-<div class="container mx-auto h-full flex flex-1 justify-center items-center">
-    <div class="w-full max-w-lg">
-      <div class="leading-loose">
-        <form class="max-w-sm m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
-            <p class="text-white text-center text-lg font-bold">LOGIN</p>
-              <div class="">
-                <label class="block text-sm text-white" for="email">Username</label>
-                <input class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white" type="username" id="username"  placeholder="Enter username" aria-label="username" required  />
-              </div>
-              <div class="mt-2">
-                <label class="block  text-sm text-white">Password</label>
-                 <input class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
-                  type="password" id="password" placeholder="Enter password" arial-label="password" required />
-              </div>
 
-              <div class="mt-4 items-center flex justify-between">
-                <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded"
-                  type="submit">Login</button>
-                <a class="inline-block right-0 align-baseline font-bold text-sm text-500 text-white hover:text-red-400"
-                  href="#">Forgot Password ?</a>
-              </div>
-              <div class="text-center">
-                <a class="inline-block right-0 align-baseline font-light text-sm text-500 text-white-50  text-white  hover:text-red-400"  href="#">
-                   Create an account
-                </a>
-              </div>
+        <>
+            <div class="h-screen font-sans login bg-cover">
+                <div class="container mx-auto h-full flex flex-1 justify-center items-center">
+                    <div class="w-full max-w-lg">
+                        <div class="leading-loose">
+                            <form class="max-w-sm m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
+                                <p class="text-white text-center text-lg font-bold">Please enter your credentials</p>
+                                <div class="">
+                                    <label class="block text-sm text-white" for="email">Username</label>
+                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white" type="username" id="username" placeholder="Enter username" aria-label="username" required />
+                                </div>
+                                <div class="mt-2">
+                                    <label class="block  text-sm text-white">Password</label>
+                                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-300 rounded focus:outline-none focus:bg-white"
+                                        type="password" id="password" placeholder="Enter password" arial-label="password" required />
+                                </div>
 
-        </form>
+                                <div class="mt-4 items-center flex justify-between">
+                                    <button class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded"
+                                        type="submit">Login</button>
+                                    <a class="inline-block right-0 align-baseline font-bold text-sm text-500 text-white hover:text-red-400"
+                                        href="#">Forgot Password ?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="inline-block right-0 align-baseline font-light text-sm text-500 text-white-50  text-white  hover:text-red-400" href="#">
+                                        Create an account
+                                    </a>
+                                </div>
 
-      </div>
-    </div>
-  </div>
-</div>
+                            </form>
 
-               
-            </>
-            
-          
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+
+
     );
 }
 
