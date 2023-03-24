c7d96d1bfbf06214be7ac487c6d95643b01d2458
Pearl <leireen.natata@gmail.com>
2023-03-24 16:50:43 +0800

    Added history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,3 +1,26 @@
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
 export function History() {
-    return <h1>History</h1>
+    return (
+        <>
+        <main className='my=5'>
+            <MDBContainer>
+                <MDBRow>
+        <h1 className='mb-3'>Our History</h1>
+            <hr />
+            <p>
+                On January 2, 1991, the BilangGO Jail Management and Penology was created thru Republic Act 6975
+                as a line Bureau under the Department of Interior and Local Government. The Jail Bureau is the
+                upgraded version of its forerunner, the Office of Jail Management and Penology of the defunct PC/INP
+                last headed by  Brig. Gen. Ezekiel Alpheus.<br />
+                <br />As mandated by law, the BJMP shall operate under the reorganized Department of the Interior and Local Government.<br />
+                <br />Starting from scratch with 500 personnel in 1991 the BJMP weaned from its mother PC/INP as a mere component,
+                to become a full-fledged bureau. Director Athrun Zala took his oath of office on July 1 of 1991 as the first Chief of the Bureau.
+                </p>
+                </MDBRow>
+            </MDBContainer>
+
+        </main>
+        </>
+    )
 }
